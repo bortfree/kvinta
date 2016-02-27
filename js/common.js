4960b580dcd0e69ea0ebcf888a4f831321e417cb
Evgeny Bortnikov <ebortnikov@mail.ru>
2016-02-27 09:13:06 +0300

    50%

---
+++ js/common.js
@@ -0,0 +1,83 @@
+$(function() {
+
+	var note = $('#note'),
+	ts = new Date(2012, 0, 1),
+	newYear = true;
+	if((new Date()) > ts){
+		var t = new Date();
+		var beforeTommorow = (24-t.getHours())*60*60*1000+(60-t.getMinutes())*60*1000+(60-t.getSeconds())*1000;
+		ts = (new Date()).getTime() + beforeTommorow;
+		newYear = false;
+	}
+
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "left until the new year!";
+			}
+			else {
+				message += "left to 10 days from now!";
+			}
+			
+			note.html(message);
+		}
+	});
+
+
+
+	$(".item_dir__descr").equalHeights();
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
